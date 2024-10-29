46a6c4da52f1272ddc93757172d6f0b9801e7ea9
𝓖𝓸𝓴𝓾𝓵𝓪𝓴𝓪𝓷𝓷𝓪𝓷 <gokulakannan2008cs@gmail.com>
2024-10-29 21:36:59 +0530

    Update CLOCK.py

--- CLOCK.py
+++ CLOCK.py
@@ -1,6 +1,6 @@
-from tkinter import *
+# version 1
+"""from tkinter import *
 from time import strftime
-from tkinter.ttk import *
 
 root = Tk()
 root.title("Clock")
@@ -16,5 +16,27 @@ def time():
 
 label = Label(root, font=("ds-digital",120), background="black", foreground="white")
 label.pack(anchor="center")
+time()
+root.mainloop()
+"""
+# version 2
+from customtkinter import *
+from time import strftime
+
+root = CTk()
+root.title("Clock")
+root.geometry("400x150")
+
+
+def time():
+    string = strftime('%H:%M:%S:%p')
+    label.configure(text=string)
+    label.after(1000, time)
+
+
+frame1 = CTkFrame(root, height=100, width=350, fg_color="azure")
+frame1.place(x=20, y=20)
+label = CTkLabel(frame1, font=("ds-digital", 55), bg_color="black", fg_color="azure")
+label.place(x=20, y=20)
 time()
 root.mainloop()
